80c289c1ec637d33253284ee51997a295ec9eea7
Shakkir Moulana <shakkirmln@gmail.com>
2022-03-08 15:41:03 +0530

    Completed Milestone 1

--- src/main/java/YamlParser.java
+++ src/main/java/YamlParser.java
@@ -1,59 +1,58 @@
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.*;
-import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class YamlParser {
-    public static void printTimeNow() {
+
+    public static void delay(int t) throws InterruptedException {
+        Thread.sleep(t*1000);
+    }
+
+    public static void printTimeNow() throws InterruptedException {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.000000;");
         LocalDateTime now = LocalDateTime.now();
         System.out.print(dtf.format(now));
     }
 
-    public static void getActivities(LinkedHashMap<String, Object> data, String val, String subval) {
+    public static void getActivities(LinkedHashMap<String, Object> data, String val, String subval) throws InterruptedException {
         for (String keys : data.keySet()) {
-            if(val.length() != 0) {
-                LinkedHashMap<String, Object> subActivity = (LinkedHashMap<String, Object>) data.get(keys);
-                LinkedHashMap<String, String> function = (LinkedHashMap<String, String>) subActivity.get("Inputs");
-                val = val + "." + keys;
+            LinkedHashMap<String, Object> subActivity = (LinkedHashMap<String, Object>) data.get(keys);
+
+            if(subActivity.get("Type").equals("Flow")) {
+                if(val.length() != 0)
+                    val = val + "." + keys;
+                else
+                    val = keys;
                 printTimeNow();
                 System.out.print(val + " Entry\n");
-                if(function != null) {
-                    subval = " Executing " + subActivity.get("Function") + " (" + function.get("FunctionInput") + ", " + function.get("ExecutionTime") + ")";
-                    printTimeNow();
-                    System.out.print(val + subval + "\n");
-                }
-            }
-            else {
-                val = keys;
-                printTimeNow();
-                System.out.print(val + " Entry\n");
-            }
-            LinkedHashMap<String, Object> linkedHashMap = (LinkedHashMap<String, Object>)data.get(keys);
-            if(linkedHashMap.get("Type").equals("Flow")) {
-                if(linkedHashMap.get("Execution").equals("Sequential")) {
-                    LinkedHashMap<String, Object> activityList = (LinkedHashMap<String, Object>) linkedHashMap.get("Activities");
+                if(subActivity.get("Execution").equals("Sequential")) {
+                    LinkedHashMap<String, Object> activityList = (LinkedHashMap<String, Object>) subActivity.get("Activities");
                     getActivities(activityList, val, subval);
+                    printTimeNow();
+                    System.out.print(val + " Exit\n");
                 }
                 else {
 
                 }
             }
             else {
+                    LinkedHashMap<String, String> function = (LinkedHashMap<String, String>) subActivity.get("Inputs");
+                    printTimeNow();
+                    System.out.print(val + "." + keys + " Entry\n");
+                    subval = " Executing " + subActivity.get("Function") + " (" + function.get("FunctionInput") + ", " + function.get("ExecutionTime") + ")";
+                    printTimeNow();
+                    System.out.print(val + "." + keys + subval + "\n");
+                    delay(Integer.parseInt(function.get("ExecutionTime")));
+                    printTimeNow();
+                    System.out.print(val + "." + keys + " Exit\n");
             }
-            printTimeNow();
-            System.out.print(val + " Exit\n");
         }
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         String path = "C:\\Files\\Projects\\KLA-Hackathon\\DataSet\\Milestone1\\Milestone1A.yaml";
 
         InputStream inputStream = new FileInputStream(path);
