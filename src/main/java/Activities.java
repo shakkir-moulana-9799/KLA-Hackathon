2c14cdc4ab7dc766f1c3caf24ba793c75134a05f
Shakkir Moulana <shakkirmln@gmail.com>
2022-03-08 13:38:41 +0530

    Rectifying the parser

--- src/main/java/Activities.java
+++ src/main/java/Activities.java
@@ -1,7 +1,21 @@
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Activities {
     private String Type;
     private String Function;
     private String Input;
+    private String Execution;
+    private Map<String, Activities> Activities = new LinkedHashMap<String, Activities>();
+
+    public Map<String, Activities> getActivities() {
+        return Activities;
+    }
+
+    public String getExecution() {
+        return Execution;
+    }
 
     public String getType() {
         return Type;
@@ -25,5 +39,13 @@ public class Activities {
 
     public void setInput(String input) {
         this.Input = input;
+    }
+
+    public void setExecution(String execution) {
+        Execution = execution;
+    }
+
+    public void setActivities(Map<String, Activities> activities) {
+        Activities = activities;
     }
 }
--- src/main/java/YamlParser.java
+++ src/main/java/YamlParser.java
@@ -2,29 +2,50 @@ import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.io.*;
+import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class YamlParser {
+    public static void printTimeNow() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.000000;");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.print(dtf.format(now));
+    }
+
+    public static void getActivities(Map<String, Object> data) {
+        for (Map.Entry<String, Object> entry : data.entrySet()) {
+            printTimeNow();
+            System.out.print(entry.getKey() + "\n");
+            LinkedHashMap<String, Object> linkedHashMap = (LinkedHashMap<String, Object>)entry.getValue();
+            if(linkedHashMap.get("Type").equals("Flow")) {
+                if(linkedHashMap.get("Execution").equals("Sequential")) {
+                    LinkedHashMap<String, Object> activityList = (LinkedHashMap<String, Object>) linkedHashMap.get("Activities");
+                    System.out.println(activityList);
+                    for (String key : activityList.keySet()) {
+                        printTimeNow();
+                        System.out.println(entry.getKey() + "." + key + " Executing " + linkedHashMap.get(key));
+                    }
+                }
+                else {
+
+                }
+            }
+            else {
+            }
+        }
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
-        String path = "C:\\Files\\Projects\\KLA-Hackathon\\DataSet\\Examples\\Milestone1\\Milestone1_Example.yaml";
+        String path = "C:\\Files\\Projects\\KLA-Hackathon\\DataSet\\tempdata.yaml";
 
         InputStream inputStream = new FileInputStream(path);
         Yaml yaml = new Yaml();
-        Map<String, WorkFlow> data = yaml.load(inputStream);
-        System.out.println(data);
-        for (Map.Entry<String, WorkFlow> entry : data.entrySet()) {
-            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.000000;");
-            LocalDateTime now = LocalDateTime.now();
-            System.out.print(dtf.format(now));
-            System.out.print(entry.getKey() + " Entry\n");
-            if(entry.getValue().getType() == "Flow") {
-
-            }
-            else {
-                System.out.println("Task");
-            }
-        }
+        Map<String, Object> data = yaml.load(inputStream);
+        getActivities(data);
     }
 }
